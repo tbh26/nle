65cfd0841cfca203f3e8465599982c83a742e1e5
Tjeerd Hes <tjeerd.hes@amsteldiep.nl>
2022-04-01 23:34:25 +0200

    [backend_commence] webApp update, handle initial "ping"

--- backend/app/application.go
+++ backend/app/application.go
@@ -1,7 +1,27 @@
 package app
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"log"
+)
+
+var (
+	router = gin.Default()
+)
+
+func pingHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "pong",
+	})
+}
 
 func StartWebApp() {
-	fmt.Println("starting webapp...")
+	router.GET("/api/ping", pingHandler)
+	address := "0.0.0.0:8765"
+	fmt.Printf("Starting webapplication at address %q. \n", address)
+	e := router.Run(address)
+	if e != nil {
+		log.Printf("failure: %q ", e.Error())
+	}
 }
--- backend/main.go
+++ backend/main.go
@@ -2,12 +2,8 @@ package main
 
 import (
 	"backend/nle/app"
-	"fmt"
 )
 
 func main() {
-	fmt.Println("Hello, world.")
-
 	app.StartWebApp()
-
 }
